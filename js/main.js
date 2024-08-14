2818879102be1ef449b77e6af42e541015e5ec7b
LauraAmaya08 <laurasofiaamayacasado@gmail.com>
2024-08-13 23:16:43 -0500

    feat: :sparkles: Main page finished

--- js/main.js
+++ js/main.js
@@ -1,19 +1,34 @@
-const elementoLista = document.querySelectorAll(".lista__boton--click");
 
-elementoLista.forEach(element => {
-    element.addEventListener("click",()=>{
-        element.classList.toggle('arrow')
-        let height = 0;
-        let menu= element.nextElementSibling
-        console.log(menu.scrollHeight)
-        if (menu.clientHeight == "0"){
-            height = menu.scrollHeight
-        }
-        menu.style.height = height+"px"
+document.addEventListener('DOMContentLoaded', () => {
+    const elementoLista = document.querySelectorAll(".lista__boton--click");
+
+    elementoLista.forEach(element => {
+        element.addEventListener("click", () => {
+            element.classList.toggle('arrow')
+            let height = 0;
+            let menu = element.nextElementSibling
+            console.log(menu.scrollHeight)
+            if (menu.clientHeight == 0) {
+                height = menu.scrollHeight
+                menu.style.height = height + "px"
+            } else {
+                menu.style.height = "0px"
+            }
+            
+        })
+    });
+
+    const abrirMenu = document.getElementById("abrir")
+    const cerrarMenu = document.getElementById("cerrar")
+
+    abrirMenu.addEventListener("click", () => {
+        console.log("ok")
+        let menu = document.querySelector(".navegador");
+        menu.classList.toggle("visible")
     })
-});
 
-const abrirMenu = document.getElementById("abrir")
-abrirMenu.addEventListener("click",()=>{
-    
-})
+    cerrarMenu.addEventListener("click", () => {
+        let menu = document.querySelector(".navegador");
+        menu.classList.remove("visible")
+    });
+});
