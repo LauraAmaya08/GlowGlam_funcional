74a74a9063b9f888278e630c70995ec5e00aa379
LauraAmaya08 <laurasofiaamayacasado@gmail.com>
2024-08-17 14:34:29 -0500

    feat: :sparkles: Each brand's page completed

--- js/main.js
+++ js/main.js
@@ -53,24 +53,7 @@ const cargarJson = async() => {
         const respuesta = await fetch("../productos.json")
         if (respuesta.ok){
             const datos = await respuesta.json();
-            const productosArray = datos.productos
-
-            let productos = "";
-            productosArray.forEach(element => {
-                let precio = element.precio
-                productos += `<div class = "producto">
-                <a href="../index.html" class="productoEnlace">
-                <img class= "imagenProducto" src= "../${element.imagen}" >
-                <div class="info">
-                <h2 class= "nombreProducto"> ${element.nombre}</h2>
-                <h3 class= "precioProducto"> $${precio.toFixed(2)} </h3>
-                <h3 class= "marcaNombre"> ${element.marca}</h3>
-                </div>
-                </a>
-                </div>
-                `
-            });
-            document.getElementById("productos").innerHTML = productos;
+            return productosArray = datos.productos
         }else{
             throw new Error ("Error al conectar con la base de datos")
         }
@@ -79,6 +62,149 @@ const cargarJson = async() => {
     }
 }
 
-cargarJson()
+const cargarTodosLosProductos = async() =>{
+    await cargarJson()
+    try {
+        let productos = "";
+        productosArray.forEach(element => {
+            let precio = element.precio
+            productos += `<div class = "producto">
+            <a href="../index.html" class="productoEnlace">
+            <img class= "imagenProducto" src= "../${element.imagen}" >
+            <div class="info">
+            <h2 class= "nombreProducto"> ${element.nombre}</h2>
+            <h3 class= "precioProducto"> $${precio.toFixed(2)} </h3>
+            <h3 class= "marcaNombre"> ${element.marca}</h3>
+            </div>
+            </a>
+            </div>
+            `
+        });
+        document.getElementById("productos").innerHTML = productos;
+    } catch (error) {
+        console.error("Error:", error.message);
+    }
+}
 
+const cargarProductosFBM = async() =>{
+    await cargarJson()
+    try {
+        let productos = "";
+        const listaFBM = [];
+        productosArray.forEach(element => {
+            if (element.marca === "Florence By Mills"){
+                listaFBM.push(element)
+            }
+        });
+        listaFBM.forEach(element => {
+            let precio = element.precio
+            productos += `<div class = "producto">
+            <a href="../index.html" class="productoEnlace">
+            <img class= "imagenProducto" src= "../${element.imagen}" >
+            <div class="info">
+            <h2 class= "nombreProducto"> ${element.nombre}</h2>
+            <h3 class= "precioProducto"> $${precio.toFixed(2)} </h3>
+            </div>
+            </a>
+            </div>
+            `
+        });
+        document.getElementById("productosFBM").innerHTML= productos
+    } catch (error) {
+        console.error("Error:", error.message);
+    }
+}
+
+const cargarProductosFB = async() =>{
+    await cargarJson()
+    try {
+        let productos = "";
+        const listaFB = [];
+        productosArray.forEach(element => {
+            if (element.marca === "Fenty Beauty"){
+                listaFB.push(element)
+            }
+        });
+        listaFB.forEach(element => {
+            let precio = element.precio
+            productos += `<div class = "producto">
+            <a href="../index.html" class="productoEnlace">
+            <img class= "imagenProducto" src= "../${element.imagen}" >
+            <div class="info">
+            <h2 class= "nombreProducto"> ${element.nombre}</h2>
+            <h3 class= "precioProducto"> $${precio.toFixed(2)} </h3>
+            </div>
+            </a>
+            </div>
+            `
+        });
+        document.getElementById("productosFB").innerHTML= productos
+    } catch (error) {
+        console.error("Error:", error.message);
+    }
+}
+
+const cargarProductosRB = async() =>{
+    await cargarJson()
+    try {
+        let productos = "";
+        const listaRB = [];
+        productosArray.forEach(element => {
+            if (element.marca === "Rare Beauty"){
+                listaRB.push(element)
+            }
+        });
+        listaRB.forEach(element => {
+            let precio = element.precio
+            productos += `<div class = "producto">
+            <a href="../index.html" class="productoEnlace">
+            <img class= "imagenProducto" src= "../${element.imagen}" >
+            <div class="info">
+            <h2 class= "nombreProducto"> ${element.nombre}</h2>
+            <h3 class= "precioProducto"> $${precio.toFixed(2)} </h3>
+            </div>
+            </a>
+            </div>
+            `
+        });
+        document.getElementById("productosRB").innerHTML= productos
+    } catch (error) {
+        console.error("Error:", error.message);
+    }
+}
+
+const cargarProductosRhode = async() =>{
+    await cargarJson()
+    try {
+        let productos = "";
+        const listaRhode = [];
+        productosArray.forEach(element => {
+            if (element.marca === "Rhode"){
+                listaRhode.push(element)
+            }
+        });
+        listaRhode.forEach(element => {
+            let precio = element.precio
+            productos += `<div class = "producto">
+            <a href="../index.html" class="productoEnlace">
+            <img class= "imagenProducto" src= "../${element.imagen}" >
+            <div class="info">
+            <h2 class= "nombreProducto"> ${element.nombre}</h2>
+            <h3 class= "precioProducto"> $${precio.toFixed(2)} </h3>
+            </div>
+            </a>
+            </div>
+            `
+        });
+        document.getElementById("productosRhode").innerHTML= productos
+    } catch (error) {
+        console.error("Error:", error.message);
+    }
+}
+
+cargarTodosLosProductos()
+cargarProductosFBM()
+cargarProductosFB()
+cargarProductosRB()
+cargarProductosRhode()
 })
